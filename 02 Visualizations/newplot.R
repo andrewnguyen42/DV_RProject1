f73fa381587531740b9add4efe3a4a22bf7eb93d
Andrew Nguyen <andrew.nguyen42@gmail.com>
2015-09-13 12:23:13 -0500

    added a shiny new visualization

--- 02 Visualizations/newplot.R
+++ 02 Visualizations/newplot.R
@@ -1,6 +1,29 @@
-png("./../00\ Doc/newplot.png",width=800,height=550,units="px")
+png("./00\ Doc/newplot.png",width=800,height=550,units="px")
 
-df <- read.csv("./../01\ Data/titanic_sex.csv")
+df <- read.csv("./01\ Data/titanic_sex.csv")
+
+df <- df[df$AGE!='null' ,]
+
+summary(as.numeric(df$AGE))
+
+ageRange <- c()
+
+for (i in 1:length(df$AGE)) {
+  age <- as.numeric(as.character(df$AGE))[i]
+
+  
+  if(age<=22)
+    ageRange<-c(ageRange,0)
+  if(age>22 && age <= 34)
+    ageRange<-c(ageRange,1)
+  if(age>34 && age <= 49)
+    ageRange<-c(ageRange,2)
+  if(age>49)
+    ageRange<-c(ageRange,3)
+}
+
+df <- data.frame(df,ageRange)
+
 
 require(extrafont)
 ggplot() +
@@ -8,9 +31,9 @@ ggplot() +
     scale_x_continuous() +
     scale_y_continuous() +
     labs(title='Titanic') +
-    labs(x="SURVIVED", y=paste("AGE")) +
+    labs(y="SURVIVED", x=paste("AGE QUARTILE")) +
     layer(data=df,
-        mapping=aes(x=as.numeric(as.character(SURVIVED)), y=as.numeric(as.character(AGE)), color=SEX),
+        mapping=aes(y=as.numeric(as.character(SURVIVED)), x=as.numeric(as.character(ageRange)), color=SEX),
         stat="identity",
         stat_params=list(),
         geom="point",
